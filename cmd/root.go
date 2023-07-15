c97059f8a3f830bb8c7d40485a5d6d4acabd2c0d
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2023-07-15 19:02:20 +0200

    Enh: Root cmd flags and logger initialization

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	nestedFormatter "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/Krzysztofz01/video-lightning-detector/internal"
@@ -19,13 +21,12 @@ var rootCmd = &cobra.Command{
 var (
 	InputVideoPath      string
 	OutputDirectoryPath string
-	detectorOptions     internal.DetectorOptions
+	VerboseMode         bool
+	DetectorOptions     internal.DetectorOptions = internal.GetDefaultDetectorOptions()
 )
 
 func init() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
-
-	defaultOptions := internal.GetDefaultDetectorOptions()
 
 	rootCmd.PersistentFlags().StringVarP(&InputVideoPath, "input-video-path", "i", "", "Input video to perform the lightning detection.")
 	rootCmd.MarkPersistentFlagRequired("input-video-path")
@@ -33,28 +34,67 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&OutputDirectoryPath, "output-directory-path", "o", "", "Output directory to store detected frames.")
 	rootCmd.MarkPersistentFlagRequired("output-directory-path")
 
-	rootCmd.PersistentFlags().Float64VarP(&detectorOptions.FrameDifferenceThreshold, "difference-threshold", "d", defaultOptions.FrameDifferenceThreshold, "The threshold used to determine the difference between two neighbouring frames.")
+	rootCmd.PersistentFlags().BoolVarP(&VerboseMode, "verbose", "v", false, "Enable verbose logging.")
 
-	rootCmd.PersistentFlags().Float64VarP(&defaultOptions.FrameBrightnessThreshold, "brightness-threshold", "b", defaultOptions.FrameBrightnessThreshold, "The threshold used to determine the brightness of the frame.")
+	rootCmd.PersistentFlags().Float64VarP(
+		&DetectorOptions.FrameDifferenceThreshold,
+		"difference-threshold", "d",
+		DetectorOptions.FrameDifferenceThreshold,
+		"The threshold used to determine the difference between two neighbouring frames.")
+
+	rootCmd.PersistentFlags().Float64VarP(
+		&DetectorOptions.FrameBrightnessThreshold,
+		"brightness-threshold", "b",
+		DetectorOptions.FrameBrightnessThreshold,
+		"The threshold used to determine the brightness of the frame.")
+
+	rootCmd.PersistentFlags().BoolVarP(
+		&DetectorOptions.SkipFramesExport,
+		"skip-frames-export", "f",
+		DetectorOptions.SkipFramesExport,
+		"Value indicating if the detected frams should not be exported.")
+
+	rootCmd.PersistentFlags().BoolVarP(
+		&DetectorOptions.SkipRaportExport,
+		"skip-raport-export", "r",
+		DetectorOptions.SkipRaportExport,
+		"Value indicating if the frames statistics raport should not be exported.")
+
+	logrus.SetLevel(logrus.InfoLevel)
+	logrus.SetOutput(os.Stdout)
+	logrus.SetFormatter(&nestedFormatter.Formatter{
+		TimestampFormat:  "",
+		HideKeys:         true,
+		NoColors:         false,
+		NoFieldsColors:   false,
+		NoFieldsSpace:    false,
+		ShowFullLevel:    false,
+		NoUppercaseLevel: false,
+		TrimMessages:     false,
+		CallerFirst:      false,
+	})
 }
 
 func Execute(args []string) {
 	rootCmd.SetArgs(args)
 	if err := rootCmd.Execute(); err != nil {
-		// TODO: Logger
-		os.Exit(1)
+		logrus.Fatal(err.Error())
 	}
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	// TODO: Validate paths
-	detector, err := internal.CreateDetector(detectorOptions)
+	if VerboseMode {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	detector, err := internal.CreateDetector(DetectorOptions)
 	if err != nil {
 		return fmt.Errorf("cmd: failed to create the detector instance: %w", err)
 	}
 
 	_, err = detector.Run(InputVideoPath, OutputDirectoryPath)
 	if err != nil {
+		logrus.Error(err)
 		return fmt.Errorf("cmd: detector run failed: %w", err)
 	}
 
