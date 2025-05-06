ca8c3febc311917e21ab3f4163b53daeff72e1f1
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2025-05-06 17:45:55 +0200

    Enh: Video binary dependencies version access and option types values access

--- internal/video/bin.go
+++ internal/video/bin.go
@@ -3,6 +3,7 @@ package video
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 const (
@@ -21,4 +22,37 @@ func AreBinariesAvailable() (bool, error) {
 	}
 
 	return true, nil
+}
+
+func GetBinariesVersions() (map[string]string, error) {
+	var (
+		binaries []string          = []string{ffmpegBinaryName, ffprobeBinaryName}
+		versions map[string]string = make(map[string]string, len(binaries))
+	)
+
+	for _, bin := range binaries {
+		cmd := exec.Command(bin, "-version")
+
+		stdout, err := cmd.Output()
+		if err != nil {
+			return nil, fmt.Errorf("video: unavailable binary %s due to version check error: %w", bin, err)
+		}
+
+		found := false
+		for _, stdoutPart := range strings.Split(string(stdout), "\n") {
+			if !strings.HasPrefix(stdoutPart, bin) {
+				continue
+			}
+
+			versions[bin] = stdoutPart
+			found = true
+			break
+		}
+
+		if !found {
+			return nil, fmt.Errorf("video: failed to access binary %s version value", bin)
+		}
+	}
+
+	return versions, nil
 }
