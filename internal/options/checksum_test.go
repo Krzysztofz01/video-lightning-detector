975bf06c03bd2ec1dbad27f284023f0a64326dbb
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2026-02-07 16:31:20 +0100

    Enh: More tests for options checksum and string expression parsing added

--- internal/options/checksum_test.go
+++ internal/options/checksum_test.go
@@ -6,8 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: Implement more tests
-
 func TestDefaultOptionsShouldProduceTheSameChecksum(t *testing.T) {
 	var (
 		a, b string
@@ -25,4 +23,68 @@ func TestDefaultOptionsShouldProduceTheSameChecksum(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, a, b)
+}
+
+func TestDifferentOptionsShouldProduceDifferentChecksums(t *testing.T) {
+	var (
+		a         DetectorOptions = GetDefaultDetectorOptions()
+		b         DetectorOptions = GetDefaultDetectorOptions()
+		aChecksum string
+		bChecksum string
+		err       error
+	)
+
+	// NOTE: Changing options that do not alter the checksum
+	a.ExportReport = true
+	b.ExportReport = false
+
+	aChecksum, err = CalculateChecksum(a)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, aChecksum)
+
+	bChecksum, err = CalculateChecksum(b)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, bChecksum)
+
+	assert.Equal(t, aChecksum, bChecksum)
+
+	// NOTE: Changing options that alter the checksum
+	a.AutoThresholds = true
+	b.AutoThresholds = false
+
+	aChecksum, err = CalculateChecksum(a)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, aChecksum)
+
+	bChecksum, err = CalculateChecksum(b)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, bChecksum)
+
+	assert.NotEqual(t, aChecksum, bChecksum)
+
+	// NOTE: Restoring the options that alter the checksum
+	a.AutoThresholds = false
+	b.AutoThresholds = false
+
+	aChecksum, err = CalculateChecksum(a)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, aChecksum)
+
+	bChecksum, err = CalculateChecksum(b)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, bChecksum)
+
+	assert.Equal(t, aChecksum, bChecksum)
+}
+
+func TestInvalidOptionsShouldNotCalculateChecksum(t *testing.T) {
+	options := GetDefaultDetectorOptions()
+	options.Denoise = -1
+
+	valid, _ := options.AreValid()
+	assert.False(t, valid)
+
+	checksum, err := CalculateChecksum(options)
+	assert.NotNil(t, err)
+	assert.Empty(t, checksum)
 }
